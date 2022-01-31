09862e4c8e515f45c255428f35ffec8ae3a06f94
YurimHwang <yurimeeee@naver.com>
2022-01-31 18:46:23 +0900

    Merge branch 'yurim' of D:\android\FriendsBridgeApp with conflicts.

---
+++ app/src/main/java/com/example/friendsbridgeapp/CalenderActivity.kt
@@ -0,0 +1,16 @@
+package com.example.friendsbridgeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalenderActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calender)
+
+
+    }
+}
