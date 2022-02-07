fd6cd0a8fd647d0d863d747fa673a9d1b7f71a74
1ny0ung <0828_iyiy@naver.com>
2022-02-07 21:10:58 +0900

    userData commit

--- app/src/main/java/com/example/friendsbridgeapp/MainActivity.kt
+++ app/src/main/java/com/example/friendsbridgeapp/MainActivity.kt
@@ -1,33 +1,58 @@
 package com.example.friendsbridgeapp
 
-import android.app.AlertDialog
-import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.widget.*
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
-import com.example.friendsbridgeapp.R
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    //val dataModelList = mutableListOf<DataModel>()
+    // 메모 기능 프래그먼트
     val memoFragment: Fragment = MemoFragment()
-    val myPageFragment : Fragment = MyPageFragment()
+    // 마이 페이지 - 회원 정보 조회, 로그아웃 기능 프래그먼트
+    public val myPageFragment : Fragment = MyPageFragment()
+    // 마이 페이지 - 회원 정보 수정 기능 프래그먼트
+    public val userDataEditFragment : Fragment = UserDataEditFragment()
+    // 캘린더 기능 프래그먼트
     val calendarFragment : Fragment = CalendarFragment()
+
+     // Firebase Database의 유저 정보를 가져오고, 프래그먼트로 저장할 변수
+    private var uid : String = ""
+    private var userName : String = ""
+    private var profileImgUrl : String = ""
+
+    // Firebase 로그인
+    private val loginAuth = Firebase.auth
+    // Firebase 데이터베이스
+    private val database = Firebase.database
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 파이어베이스 데이터베이스 중 유저 정보에 접근
+        val myRef = database.getReference("users")
+        // 유저 정보에서 현재 로그인 된 유저 정보에 접근
+        val userRef = myRef.child(loginAuth.currentUser!!.uid.toString())
+
+        // 현재 유저의 uid, 닉네임, 프로필 사진 저장 경로를 각각의 변수에 업데이트해 저장
+        userRef.addValueEventListener(object :
+            ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                profileImgUrl = snapshot.getValue(userDataModel::class.java)!!.profileImgUrl
+                uid = snapshot.getValue(userDataModel::class.java)!!.uid
+                userName = snapshot.getValue(userDataModel::class.java)!!.userName
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        // 하단의 캘린더 버튼 누르면 캘린더 프래그먼트 호출
         val btnCalendar = findViewById<Button>(R.id.btnCalendar)
         btnCalendar.setOnClickListener {
             supportFragmentManager.beginTransaction()
@@ -35,26 +60,41 @@ class MainActivity : AppCompatActivity() {
                     .commitAllowingStateLoss()
         }
 
-
+        // 하단의 마이페이지 버튼 누르면 마이 페이지 프래그먼트 호출
         val btnMyPage = findViewById<Button>(R.id.btnMyPage)
         btnMyPage.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentView, myPageFragment)
-                .commitAllowingStateLoss()
+            // 데이터 베이스에서 저장한 유저 정보를 마이 페이지 프래그먼트로 전달
+            putUserData(myPageFragment)
+            // 마이 페이지로 이동
+            moveFragment(myPageFragment)
         }
 
+        // 하단의 메모 버튼을 누르면 메모 프래그먼트 호출
         val btnMemo = findViewById<Button>(R.id.btnMemo)
         btnMemo.setOnClickListener {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentView, memoFragment)
                 .commitAllowingStateLoss()
         }
+
     }
 
-    fun receiveUserData(what : userDataModel){
-        val userData : userDataModel = what
-        Log.d("userData", userData.userName)
-        Log.d("userData", userData.uid)
-        Log.d("userData", userData.profileImgUrl)
+    // 파이어베이스 데이터베이스에서 가져온 현재의 유저 정보를 프래그먼트들로 전달
+    fun putUserData(fragment : Fragment){
+        val bundle = Bundle()
+
+        // 현재 유저의 uid, 닉네임, 프로필 사진 경로를 bundle에 넣음
+        bundle.putString("uid", uid)
+        bundle.putString("userName", userName)
+        bundle.putString("profileImgUrl", profileImgUrl)
+
+        fragment.arguments = bundle
+    }
+
+    // 현재의 액티비티에서 프래그먼트로 이동
+    fun moveFragment(fragment : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentView, fragment)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/example/friendsbridgeapp/MyPageFragment.kt
+++ app/src/main/java/com/example/friendsbridgeapp/MyPageFragment.kt
@@ -1,34 +1,20 @@
 package com.example.friendsbridgeapp
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.core.net.toUri
 import com.bumptech.glide.Glide
-import com.google.android.gms.auth.api.Auth
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
-import java.io.File
-import java.net.URL
-import kotlin.math.log
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -40,34 +26,43 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MyPageFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+// 마이 페이지 (회원 정보 조회, 회원 정보 변경 프래그먼트로 이동, 로그아웃)
+// 그룹 기능 추가 예정
 class MyPageFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private lateinit var profileImageView: ImageView
-    private lateinit var txtNickName : TextView
-    private lateinit var txtAccount : TextView
 
+    private lateinit var profileImageView: ImageView  // 유저의 프로필 사진을 보여 줄 imageView
+    private lateinit var txtNickName : TextView // 유저의 닉네임을 보여 줄 textView
+    private lateinit var txtAccount : TextView // 유저의 가입 계정 (이메일)을 보여 줄 textView
+
+    // 회원 정보 수정, 로그아웃 버튼
     private lateinit var btnUserDataEdit : Button
     private lateinit var btnLogOut : Button
 
+    // 파이어베이스 인증 로그인 활용
     private lateinit var loginAuth : FirebaseAuth
-    private val database = Firebase.database
-    private var storage = Firebase.storage
-    lateinit var profileUrl : String
-    private final val GALLERY_CODE : Int = 10
-    private var imgFile : Uri? = null
-    private lateinit var edtNickname : EditText
-    private lateinit var txtAccount2 : TextView
-    private lateinit var profileImageView2 : ImageView
-    private lateinit var btnSaveData : Button
-    private lateinit var btnWithdraw : Button
+
+    // MainActivity에서 받아올 uid, 닉네임, 프로필 사진 저장 경로 변수
+    var profileImgUrl : String = ""
+    var userName : String = ""
+    var uid : String = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            // MainActivity에서 전달 받은 uid, 닉네임, 프로필 사진 저장 경로를 변수에 저장
+            uid = it.getString("uid").toString()
+            userName = it.getString("userName").toString()
+            profileImgUrl = it.getString("profileImgUrl").toString()
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // 뷰에 있는 컴포넌트들을 변수와 연결
         profileImageView = getView()!!.findViewById<ImageView>(R.id.profileImgView)
         txtNickName = getView()!!.findViewById<TextView>(R.id.txtNickName)
         txtAccount = getView()!!.findViewById<TextView>(R.id.txtAccount)
@@ -76,163 +71,48 @@ class MyPageFragment : Fragment() {
         btnLogOut = getView()!!.findViewById<Button>(R.id.btnLogOut)
 
         loginAuth = Firebase.auth
+
+        // 프로필 사진을 동그랗게 변경, 기본 이미지 설정
         profileImageView.setImageResource(R.drawable.ic_baseline_person_24)
         profileImageView.background = ShapeDrawable(OvalShape())
         profileImageView.clipToOutline = true
 
-        val myRef = database.getReference("users")
-        val userRef = myRef.child(loginAuth.currentUser!!.uid.toString())
-        userRef.addValueEventListener(object :
-            ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                profileUrl = snapshot.getValue(userDataModel::class.java)!!.profileImgUrl
-                txtNickName.setText(snapshot.getValue(userDataModel::class.java)!!.userName)
 
-                val mActivity = activity as MainActivity
-                mActivity.receiveUserData(snapshot.getValue(userDataModel::class.java)!!)
-                Log.d("TAG", "액티비티로 데이터 전달")
-
-                Firebase.storage.reference.child(profileUrl).downloadUrl.addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        Glide.with(getView()!!).load(it.result).override(400, 400)
-                            .thumbnail(0.1f).into(profileImageView)
-                    }
-                    else{
-                        Log.d("error", "업로드 실패")
-                        Glide.with(getView()!!).load(R.drawable.ic_baseline_person_24).override(400,400).into(profileImageView)
-                    }
-                }
-                txtAccount.setText(loginAuth.currentUser!!.email.toString())
+        // 파이어베이스 storage에서 유저 프로필 사진 저장 경로의 사진을 받아옴
+        // 성공 시 glide 활용해 imageView에 사진 적용
+        Firebase.storage.reference.child(profileImgUrl).downloadUrl.addOnCompleteListener {
+            if (it.isSuccessful) {
+                Glide.with(getView()!!).load(it.result).override(400, 400)
+                    .thumbnail(0.1f).into(profileImageView)
             }
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+            // 사진 가져오기 오류 발생 시 기본 프로필 사진 띄움
+            else{
+                Log.d("error", "업로드 실패")
+                Glide.with(getView()!!).load(R.drawable.ic_baseline_person_24).override(400,400).into(profileImageView)
             }
-
-        })
-
-        btnUserDataEdit.setOnClickListener {
-
         }
-        /*
+
+        // 유저 계정 정보 (이메일)을 파이어베이스 로그인 정보에서 가져와 텍스트로 보여 줌.
+        txtAccount.setText(loginAuth.currentUser!!.email.toString())
+        // 유저 닉네임 텍스트에 userName 값 적용
+        txtNickName.setText(userName)
+
+        // 회원 정보 수정 버튼을 누르는 경우
         btnUserDataEdit.setOnClickListener {
-            val mDialogView = LayoutInflater.from(context).inflate(R.layout.userdataedit_dialog, null)
-            val mBuilder = AlertDialog.Builder(context)
-                .setView(mDialogView)
-                .setTitle("회원 정보 관리")
-            val mAlertDialog = mBuilder.show()
-            edtNickname = mAlertDialog.findViewById<EditText>(R.id.edtNickName)
-            txtAccount2 = mAlertDialog.findViewById<TextView>(R.id.txtAccount)
-            profileImageView2 = mAlertDialog.findViewById<ImageView>(R.id.profileImgView)
-            profileImageView2.setImageResource(R.drawable.ic_baseline_person_24)
-            profileImageView2.background = ShapeDrawable(OvalShape())
-            profileImageView2.clipToOutline = true
-            /*
-            Firebase.storage.reference.child(profileUrl).downloadUrl.addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Glide.with(mDialogView).load(it.result).override(400, 400)
-                        .thumbnail(0.1f).into(profileImageView2)
-                }
-                else{
-                    Log.d("error", "업로드 실패")
-                    Glide.with(mDialogView).load(R.drawable.ic_baseline_person_24).override(400,400).into(profileImageView2)
-                }
-            }
-
-             */
-            /*
-            btnSaveData = mAlertDialog.findViewById<Button>(R.id.btnSaveData)
-            btnWithdraw = mAlertDialog.findViewById<Button>(R.id.btnWithdraw)
-
-            edtNickname.setText(txtNickName.text.toString())
-            txtAccount2.setText(txtAccount.text.toString())
-            profileImageView2.setOnClickListener {
-                val intent : Intent = Intent(Intent.ACTION_PICK)
-                intent.setType(MediaStore.Images.Media.CONTENT_TYPE)
-                startActivityForResult(intent, GALLERY_CODE)
-            }*/
-            /*
-            btnSaveData.setOnClickListener {
-                val databaseRef = database.reference.child("users").child(loginAuth.currentUser!!.uid)
-                val userStorageReference = storage.reference.child("userImgs").child( "ProfileImg/" + loginAuth.currentUser!!.uid)
-                databaseRef.addValueEventListener(object : ValueEventListener{
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                            val userDB: userDataModel? =
-                                snapshot.getValue(userDataModel::class.java)
-                            if (userDB!!.profileImgUrl == "/userImgs/ProfileImg/defaultprofileImg.jpg") {
-                                userStorageReference.putFile(imgFile!!)
-                                    .addOnCompleteListener { task ->
-                                        if (task.isSuccessful) {
-                                            val userDataModel = userDataModel(
-                                                edtNickname.text.toString(),
-                                                userStorageReference.path.toString()
-                                            )
-                                            databaseRef
-                                                .push()
-                                                .setValue(userDataModel)
-                                            mAlertDialog.dismiss()
-                                        } else {
-                                            Log.d("check", "프로필 사진 업로드 실패")
-                                            mAlertDialog.dismiss()
-                                        }
-                                    }
-                            } else {
-                                userStorageReference.delete().addOnCompleteListener { task ->
-                                    if(task.isSuccessful){
-                                        userStorageReference.putFile(imgFile!!).addOnCompleteListener { task->
-                                            if(task.isSuccessful){
-                                                val userDataModel = userDataModel(
-                                                    edtNickname.text.toString(),
-                                                    userStorageReference.path.toString()
-                                                )
-                                                databaseRef
-                                                    .setValue(userDataModel)
-                                                mAlertDialog.dismiss()
-                                            }
-                                            else{
-                                                Log.d("check", "새로운 프로필 사진 업로드 실패")
-                                            }
-                                        }
-                                    }
-                                    else{
-                                        Log.d("check", "기존 프로필 사진 삭제 실패")
-                                        mAlertDialog.dismiss()
-                                    }
-                                }
-                            }
-                        }
-
-                    override fun onCancelled(error: DatabaseError) {
-                        TODO("Not yet implemented")
-                    }
-                })
-
-                Firebase.storage.reference.child("userImgs/ProfileImg/" + loginAuth.currentUser!!.uid.toString()).delete().addOnCompleteListener {task->
-                    if(task.isSuccessful) {
-                        Firebase.storage.reference.child("userImgs/ProfileImg/" + loginAuth.currentUser!!.uid.toString())
-                            .putFile(imgFile!!).addOnCompleteListener { task ->
-                                if (task.isSuccessful) {
-                                    database.reference.child("users").child(loginAuth.currentUser!!.uid)
-                                        .child("profileImgUrl")
-                                        .setValue("userImgs/ProfileImg/" + loginAuth.currentUser!!.uid.toString())
-                                }
-                                else{
-                                Log.d("check", "올리기 실패")
-                                }
-                        }
-                    }
-                    else{
-                        Log.d("check", "삭제 실패")
-                    }
-                }
-            }*/
-        }*/
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == GALLERY_CODE) run {
-            imgFile = data!!.data
-            profileImageView2.setImageURI(imgFile)
+            // 메인 액티비티에서 미리 만들어 둔 회원 정보 수정 프래그먼트로 이동해야 함.
+            // 실시간 데이터 베이스에서 메인으로 전달한 데이터를 받아야 하기 때문.
+            val mActivity = activity as MainActivity
+            mActivity.putUserData(mActivity.userDataEditFragment)
+            mActivity.moveFragment(mActivity.userDataEditFragment)
         }
+
+        // 로그아웃 버튼을 누르는 경우
+        btnLogOut.setOnClickListener {
+            // 로그아웃 후 다시 Splash 화면으로 이동
+            loginAuth.signOut()
+            startActivity(Intent(this.context, SplashActivity::class.java))
+        }
+
     }
 
     override fun onCreateView(
