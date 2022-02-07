8da9d64ac163eb5150fd9296bc902d1d7d5604f0
irankrap <68219384+irankrap@users.noreply.github.com>
2022-02-07 11:26:53 +0900

    commit 오류 해결

--- app/src/main/java/com/example/friendsbridgeapp/SplashActivity.kt
+++ app/src/main/java/com/example/friendsbridgeapp/SplashActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -12,6 +15,8 @@ import com.google.firebase.ktx.Firebase
 import java.lang.Exception
 
 class SplashActivity : AppCompatActivity() {
+
+    var state: Int = 1
 
     private lateinit var auth: FirebaseAuth
 
@@ -21,13 +26,15 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        var trainImage = findViewById(R.id.train_image) as ImageView
+
+        var animation: Animation = AnimationUtils.loadAnimation(this, R.anim.train_move)
+        trainImage.startAnimation(animation)
+
+
+
         auth = Firebase.auth
 
-        Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }, 3000)
-        /*
         try {
 
             Log.d("SPLASH", auth.currentUser!!.uid)
@@ -35,7 +42,7 @@ class SplashActivity : AppCompatActivity() {
             Handler().postDelayed({
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
-            }, 3000)
+            }, 5000)
 
         } catch (e : Exception) {
             Log.d("SPLASH", "회원가입 시켜줘야함")
@@ -49,7 +56,7 @@ class SplashActivity : AppCompatActivity() {
                             Handler().postDelayed({
                                 startActivity(Intent(this, MainActivity::class.java))
                                 finish()
-                            }, 3000)
+                            }, 5000)
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(this, "비회원 로그인 실패", Toast.LENGTH_LONG).show()
@@ -58,8 +65,6 @@ class SplashActivity : AppCompatActivity() {
 
         }
 
-         */
+
     }
-
-
 }
